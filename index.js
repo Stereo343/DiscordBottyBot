2a19d1def1584dc76cc48f9207a54e207ca215ec
Bruno Cornet <cornetbruno343@gmail.com>
2020-06-03 13:34:21 +0200

    test

--- index.js
+++ index.js
@@ -1,14 +1,40 @@
+require('dotenv').config();
 const Discord = require('discord.js');
+const mysql = require('mysql');
+
 const client = new Discord.Client();
+let connection = mysql.createConnection({
+	host: process.env.DB_HOST,
+	user: process.env.DB_USER,
+	pasword: process.env.DB_PASSWORD,
+	database: process.env.DB_NAME,
+	charset: 'utf8mb4_general_ci'
+});
+
+connection.connect((err) => {
+	if (err) throw err;
+	console.log("Connected!")
+});
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('Pong!');
+  if (msg.content === 'is America safe yet ?') {
+    msg.reply('Nope');
+  }
+  else {
+  	let sql = "INSERT INTO messages (user, content, time) VALUES ?";
+  	let date = new Date();
+  	let values = [
+  		[msg.author, msg.content, date]
+  	]
+  	connection.query(sql, [values], function (err, result) {
+	    if (err) throw err;
+	    console.log("1 record inserted");
+	});
   }
 });
 
-client.login(process.env.discord_token);
+client.login(process.env.DISCORD_TOKEN);
